428a97c10aa11a81a9910da4472e34d145e48801
Nawras Albarwani <nawras@mail.usf.edu>
2020-02-06 18:48:57 -0500

    added comments

--- Albarwany_Methods/Program.cs
+++ Albarwany_Methods/Program.cs
@@ -19,15 +19,32 @@ namespace Albarwany_Methods
                 // variable used to gather data from user input. 
                 string input = Console.ReadLine();
 
-                // Method to prints " hello user name!
-                Console.WriteLine("Hello " + input + "!");
+                // if else statment to check user input
+                // check to see if users input data is a string without numbers or symbols 
+                if ((System.Text.RegularExpressions.Regex.IsMatch(input, "[a-zA-Z]$" + " " + "[a-zA-Z]$")) || ((System.Text.RegularExpressions.Regex.IsMatch(input, "[a-zA-Z]$"))))
+                {
+                    HelloUser(ref input);
+                }
+                else
+                {
+                    // else , if input is not a sting display error message 
+                    Console.WriteLine("Please enter a name consisting of all string values. No symbols or numeric digits ");
 
+                }// end of else
             } // end of try 
             catch
             {
                 Console.WriteLine("Please enter a string value");
-                Console.ReadKey(true);
+
             }// end of catch
         }
+            // Method to prints " hello user name!
+            private static void HelloUser(ref string input)
+            {
+                // Display message 
+                Console.WriteLine("Hello " + input + "!");
+            
+            }
+        }
     }
-}
+
